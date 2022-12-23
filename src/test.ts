88bb2f69468ea4ab4d225527db9cfe07fee4e31b
Aleksey Romanov <aromanovnik@gmail.com>
2022-12-23 23:50:20 +0300

    lint, stylelint and prettier

--- src/test.ts
+++ src/test.ts
@@ -1,11 +1,8 @@
-var foo = 1
-console.log(foo)
-var bar
-bar = 1
-function test(
-
-
-) {
-  console.log(baz)
+const foo = 1;
+console.log(foo);
+let bar;
+bar = 1;
+function test() {
+  console.log(baz);
 }
-var baz = 123
+var baz = 123;
