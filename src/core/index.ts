b5e222fc2e0313323ea65830a4096575a4779e8b
Aleksey Romanov <aromanovnik@gmail.com>
2022-12-26 00:03:01 +0300

    Block

---
+++ src/core/index.ts
@@ -0,0 +1,3 @@
+export * from './Block';
+export * from './registerComponent';
+export * from './render-dom';
---
+++ src/core/render-dom.ts
@@ -0,0 +1,12 @@
+import Block from './block';
+
+export default function renderDom(block: Block) {
+  const root = document.querySelector('#app');
+
+  if (!root) {
+    throw new Error('Error: #app not found');
+  }
+
+  root.innerHTML = '';
+  root.append(block.getContent());
+}
