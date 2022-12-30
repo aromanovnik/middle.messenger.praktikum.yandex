f2da1d99259bbcdd788ecee6bc840d195f3510bc
AlekseyRomanov <aromanov@domprog.com>
2022-12-30 14:34:04 +0300

    User edit

--- src/components/user-edit/user-edit.component.ts
+++ src/components/user-edit/user-edit.component.ts
@@ -1,18 +1,90 @@
 import { Block } from 'core';
+import { userService } from 'services';
 // todo: Only for demo
-import { userInfo } from 'demo';
+import { userInfo, UserUpdateRequest } from 'demo';
 
 import './user-edit.component.css';
+import { validateForm, ValidateRuleType } from '../../helpers';
 
 export class UserEditComponent extends Block {
   static override componentName = 'UserEditComponent';
+
+  userService = userService;
+
+  formValue: UserUpdateRequest = {
+    firstName: '',
+    secondName: '',
+    displayName: '',
+    login: '',
+    email: '',
+    phone: '',
+  };
 
   constructor() {
     super();
 
     this.setProps({
       user: userInfo,
+      error: '',
+      values: this.formValue,
+      onSubmit: this.onSubmit.bind(this),
+      onBlur: this.onBlur.bind(this),
+      onInput: this.onInput.bind(this),
     });
+  }
+
+  validate(): string {
+    const message = validateForm([
+      {
+        type: ValidateRuleType.Name,
+        value: this.formValue.firstName,
+      },
+      {
+        type: ValidateRuleType.Name,
+        value: this.formValue.secondName,
+      },
+      {
+        type: ValidateRuleType.Login,
+        value: this.formValue.login,
+      },
+      {
+        type: ValidateRuleType.Email,
+        value: this.formValue.email,
+      },
+      {
+        type: ValidateRuleType.Phone,
+        value: this.formValue.phone,
+      },
+    ]);
+
+    this.setProps({
+      error: message,
+      values: this.formValue,
+    });
+    return message;
+  }
+
+  onSubmit(event: MouseEvent): void {
+    event?.preventDefault();
+
+    if (this.validate()) {
+      return;
+    }
+
+    this.userService.editInfo(this.formValue);
+  }
+
+  onBlur(): void {
+    this.validate();
+  }
+
+  onInput(event: InputEvent): void {
+    const target = event.target as HTMLInputElement;
+    const name = target?.dataset['key'] ? target?.dataset['key'] : target.name;
+    if (name in this.formValue) {
+      // @ts-ignore
+      this.formValue[name] = target.value;
+    }
   }
 
   protected override render(): string {
@@ -57,6 +129,7 @@ export class UserEditComponent extends Block {
                                         id='userEditFirstName'
                                         type='text'
                                         name='first_name'
+                                        dataKey='firstName'
                                         placeholder=''
                                         value=user.firstName
                                 }}}
@@ -68,6 +141,7 @@ export class UserEditComponent extends Block {
                                         id='userEditSecondName'
                                         type='text'
                                         name='second_name'
+                                        dataKey='secondName'
                                         placeholder=''
                                         value=user.secondName
                                 }}}
@@ -79,6 +153,7 @@ export class UserEditComponent extends Block {
                                         id='userEditDisplayName'
                                         type='text'
                                         name='display_name'
+                                        dataKey='displayName'
                                         placeholder=''
                                         value=user.displayName
                                 }}}
@@ -95,6 +170,8 @@ export class UserEditComponent extends Block {
                                 }}}
                             </li>
                         </ul>
+
+                        {{{InputErrorComponent error=error}}}
 
                         {{{ButtonComponent className='user-edit__save' title='Сохранить'}}}
                     </form>
