5f0b2d3fb5fd42126d60bf576e20ac6053576c70
Aleksey Romanov <aromanovnik@gmail.com>
2023-02-01 01:23:56 +0300

    netlify

--- src/hocs/router.hoc.ts
+++ src/hocs/router.hoc.ts
@@ -1,13 +1,27 @@
 import { Router, BlockClass } from 'core';
-import router, { ScreensPath } from 'router';
+import router from 'router';
 
 export type RouterHocProps = {
   router: Router;
-  links: typeof ScreensPath;
+  links: Record<string, string>;
 };
 
 export function routerHoc<P extends RouterHocProps>(WrappedBlock: BlockClass<P>) {
-  const links: RouterHocProps['links'] = ScreensPath;
+  //
+  // const links: RouterHocProps['links'] = ScreensPath;
+  // todo: Ошибка Cannot access 'fs' before initialization, буду изучать
+  const links: RouterHocProps['links'] = {
+    Messenger: '/messenger',
+    MessengerDetail: '/messenger/:id-chat',
+    Onboarding: '/onboadring',
+    Login: '/',
+    Registration: '/sign-up',
+    Settings: '/settings',
+    SettingsPass: '/settings-pass',
+    Profile: '/profile',
+    NotFound: '/not-found',
+    ServerError: '/server-error',
+  };
 
   // @ts-expect-error No base constructor has the specified number of type arguments
   return class extends WrappedBlock<P> {
