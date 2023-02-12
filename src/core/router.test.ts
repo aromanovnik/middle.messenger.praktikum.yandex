ac46e1fc62e3b01dddf66fe3f4ee42332cb7be69
Aleksey Romanov <aromanovnik@gmail.com>
2023-02-12 23:49:58 +0300

    Added additional tests for routing

--- src/core/router.test.ts
+++ src/core/router.test.ts
@@ -10,5 +10,45 @@ describe('core/Router', () => {
     router.go(path);
 
     expect(mock).toHaveBeenCalled();
+
+    expect(window.location.pathname).toEqual(`/${path}`);
+  });
+
+  it('should show the route if the route is not found', () => {
+    const router = new Router();
+
+    const mock = jest.fn();
+    router.use('*', mock);
+
+    router.go('/');
+
+    expect(mock).toHaveBeenCalled();
+  });
+
+  it('should back in history', () => {
+    const router = new Router();
+
+    window.history.pushState({}, '', '/route1');
+    window.history.pushState({}, '', '/route2');
+
+    router.back();
+
+    window.onpopstate = (_event: PopStateEvent) => {
+      expect(window.location.pathname).toBe('/route1');
+    };
+  });
+
+  it('should forward in history', () => {
+    const router = new Router();
+
+    window.history.pushState({}, '', '/route1');
+    window.history.pushState({}, '', '/route2');
+
+    router.back();
+    router.forward();
+
+    window.onpopstate = (_event: PopStateEvent) => {
+      expect(window.location.pathname).toBe('/route2');
+    };
   });
 });
