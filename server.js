13a652400241cc76d16affc9f0a14be5327e15cc
Aleksey Romanov <aromanovnik@gmail.com>
2023-02-12 17:06:10 +0300

    Small fix

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(express.static('dist'));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+app.use(express.static('./dist/'));
+
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+  console.log(`Example app listening on port ${PORT}!`);
 });
